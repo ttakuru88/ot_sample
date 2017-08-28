01341e225d3546e94371ea1d83d82cee84a30a1c
takuru <asaumi@kray.jp>
2017-08-28 11:56:33 +0900

    index.htmlを返すサーバを起動できる

---
+++ server.js
@@ -0,0 +1,12 @@
+// https://socket.io/get-started/chat/
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
